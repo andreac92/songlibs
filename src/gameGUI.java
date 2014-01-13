ef12aa81bb82049e373a65109209d4d0819308cd
Andrea Campos <andrea.ca92@yahoo.com>
2014-01-13 00:11:34 -0800

    added source files

---
+++ src/gameGUI.java
@@ -0,0 +1,302 @@
+
+import java.awt.EventQueue;
+import javax.swing.JFrame;
+import javax.swing.JPanel;
+import java.awt.CardLayout;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import javax.swing.JComboBox;
+import javax.swing.JLabel;
+import java.awt.Font;
+import javax.swing.BorderFactory;
+import javax.swing.JScrollPane;
+import javax.swing.JTextArea;
+import javax.swing.SwingConstants;
+import javax.swing.JButton;
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.HashMap;
+import java.util.List;
+import javax.swing.JTextField;
+import java.awt.Dimension;
+
+public class gameGUI {
+
+	private JFrame frmSonglibs;
+	private JPanel cards;
+	private JPanel startPage;
+	private JPanel madlibPage;
+	private JPanel finishedPage;
+	private gameLogic chosenSong;
+	private JComboBox comboBox;
+	private JScrollPane scroller;
+	private HashMap<JTextField, String> toGet;
+	
+	/**
+	 * Creates the game based on song choice; creates end of game
+	 * through the "submit" action
+	 */
+	public void game(){
+		int i = makePlayScreen();
+		makeSubmit(i);
+	}
+	/**
+	 * Launch the application.
+	 */
+	public static void main(String[] args) {
+		EventQueue.invokeLater(new Runnable() {
+			public void run() {
+				try {
+					gameGUI window = new gameGUI();
+					window.frmSonglibs.setVisible(true);
+				} catch (Exception e) {
+					e.printStackTrace();
+				}
+			}
+		});
+	}
+
+	/**
+	 * Create the application.
+	 */
+	public gameGUI() {
+		initialize();
+	}
+
+	/**
+	 * Initialize the contents of the frame.
+	 */
+	private void initialize() {
+		makeStartPage();
+		makestartPageText();
+		makeSongOptions();
+		makeRandomButton();
+		makeMadLibPage();
+		makeFinishedPage();
+	}
+	
+	/**
+	 * 1. Get values from hashmap (keys are specific word, ie NOUN1, NOUN2; 
+	 *     value is descriptor ie a NOUN)
+	 * 2. Put these values in a label, place next to text field
+	 * 3. Put text field and key from previous hashmap in a new hashmap
+	 */
+private int makePlayScreen() {
+	toGet = new HashMap<JTextField, String>();
+	int i=5;
+	List<Object> valuesList = new ArrayList<Object>(chosenSong.replaceWords.keySet());
+	Collections.shuffle(valuesList);
+	for (Object word : valuesList) {
+		JLabel lblNewLabel_1 = new JLabel(chosenSong.replaceWords.get(word));
+		lblNewLabel_1.setBounds(5, i, 161, 16);
+		madlibPage.add(lblNewLabel_1);
+		madlibPage.revalidate();
+		madlibPage.repaint();
+		JTextField txt1 = new JTextField();
+		txt1.setBounds(180, i, 130, 30);
+		toGet.put(txt1, (String)word);
+		madlibPage.add(txt1);
+		madlibPage.revalidate();
+		madlibPage.repaint();
+		txt1.setColumns(10);
+		i+=30;
+	}
+	return i;
+}
+
+/**
+* Create the submit button
+*/
+private void makeSubmit(int i){
+	JButton jb = new JButton("Submit");
+	jb.addActionListener(new submitActions());
+	jb.setBounds(5, i, 61, 16);
+	madlibPage.add(jb);
+	madlibPage.setPreferredSize(new Dimension(400, i+40));
+	madlibPage.revalidate();
+	madlibPage.repaint();
+}
+
+/**
+* Action performed on pressing submit button:
+* 1. Get word typed into text field, place into song
+* 2. Display final song
+* 3. Display option to redo the song, and option to do another song
+*/
+public class submitActions implements ActionListener {
+	public void actionPerformed(ActionEvent e) {
+	       for (JTextField txt : toGet.keySet()) {
+		   String userWord = txt.getText();
+		   String word = toGet.get(txt);
+		   chosenSong.replaceWord(word, userWord);
+		}
+		showSong();
+		restartAction();
+		redoAction();
+		finishedPage.revalidate();
+		finishedPage.repaint();
+		CardLayout cl = (CardLayout)(cards.getLayout());
+		cl.next(cards);
+	}
+}
+/**
+* Display the finished song
+*/
+private void showSong() {
+	JLabel madlibsongName = new JLabel(chosenSong.songName);
+	madlibsongName.setBounds(155, 6, 400, 20);
+	JTextArea finishedmadlibsong = new JTextArea(chosenSong.song);
+	finishedmadlibsong.setWrapStyleWord(true);
+	finishedmadlibsong.setEditable(false);
+	finishedmadlibsong.setLineWrap(true);
+	finishedmadlibsong.setOpaque(false);
+	finishedmadlibsong.setBounds(25, 31, 390, 200);
+	JScrollPane scrollPane = new JScrollPane(finishedmadlibsong);
+	scrollPane.setBounds(25,31,420,200);
+	scrollPane.setOpaque(false);
+	scrollPane.getViewport().setOpaque(false);
+	scrollPane.setBorder(BorderFactory.createEmptyBorder());
+	finishedPage.add(madlibsongName);
+	finishedPage.add(scrollPane);
+}
+/**
+* Create the restart button, and its action to clear the song, song panel and
+* move to the start page panel.
+*/
+private void restartAction() {
+	JButton restart = new JButton("Choose another song");
+	restart.setBounds(80, 250, 160, 20);
+	restart.addActionListener(new ActionListener() {
+	public void actionPerformed(ActionEvent e) {
+	       madlibPage.removeAll();
+	       madlibPage.revalidate();
+	       madlibPage.repaint();
+	       scroller.revalidate();
+	       scroller.repaint();
+	       finishedPage.removeAll();
+	       finishedPage.revalidate();
+	       finishedPage.repaint();
+	       CardLayout cl = (CardLayout)(cards.getLayout());
+	       comboBox.setSelectedIndex(0);
+	       cl.show(cards, "home");
+	       }
+	});
+	finishedPage.add(restart);
+}
+/**
+* Create the redo button and its action to move back to the song panel
+*/
+private void redoAction() {
+	JButton redo = new JButton("Redo song");
+	redo.setBounds(250, 250, 100, 20);
+	redo.addActionListener(new ActionListener() {
+	public void actionPerformed(ActionEvent e) {
+	       chosenSong.reset();
+	       madlibPage.removeAll();
+	       madlibPage.revalidate();
+	       madlibPage.repaint();
+	       scroller.revalidate();
+	       scroller.repaint();
+	       finishedPage.removeAll();
+	       finishedPage.revalidate();
+	       finishedPage.repaint();
+	       CardLayout cl = (CardLayout)(cards.getLayout());
+	       comboBox.setSelectedIndex(0);
+	       game();
+	       cl.show(cards, "madlibsong");
+	       }
+	});
+	finishedPage.add(redo);
+}
+/**
+* Create the panel for the start page where users choose their song
+*/
+private void makeStartPage(){
+	frmSonglibs = new JFrame();
+	frmSonglibs.setTitle("SongLibs");
+	frmSonglibs.setBounds(100, 100, 450, 300);
+	frmSonglibs.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+	frmSonglibs.getContentPane().setLayout(new CardLayout(0, 0));
+	cards = (JPanel) frmSonglibs.getContentPane();
+	startPage = new JPanel();
+	frmSonglibs.getContentPane().add(startPage, "home");
+	startPage.setLayout(null);
+}
+/**
+* Create the title and instruction text for the start page
+*/
+private void makestartPageText() {
+	JLabel lblmadlibsonglibs = new JLabel("SONGLIBS");
+	lblmadlibsonglibs.setFont(new Font("Bookman Old Style", Font.PLAIN, 37));
+	lblmadlibsonglibs.setBounds(115, 6, 200, 44);
+	startPage.add(lblmadlibsonglibs);
+	JLabel lblNewLabel = new JLabel("<html>Your classic madlibs game -- with a twist! Fill in the<br>words to your favorite songs and enjoy the hilarity that ensues. Choose a song from the list, or have one chosen for you by clicking the random button.");
+	lblNewLabel.setVerticalAlignment(SwingConstants.TOP);
+	lblNewLabel.setBounds(48, 63, 374, 73);
+	startPage.add(lblNewLabel);
+	JLabel lblOr = new JLabel("- OR -");
+	lblOr.setBounds(178, 187, 61, 16);
+	startPage.add(lblOr);
+}
+/**
+* Create the drop-down menu for the song options on the start page
+*/
+private void makeSongOptions() {
+	chosenSong = new gameLogic();
+	String[] options = chosenSong.getOptions();
+	comboBox = new JComboBox(options);
+    comboBox.setEditable(false);
+	comboBox.setBounds(125, 148, 153, 27);
+	comboBox.addActionListener(new ActionListener() {
+	  @Override
+	  public void actionPerformed(ActionEvent e) {
+	    	if (((String)comboBox.getSelectedItem()).equals("")){
+	    	   return;
+	    	}
+	        chosenSong.choose((String)comboBox.getSelectedItem());
+	        chosenSong.initializeLib();
+	        game();
+	        CardLayout cl = (CardLayout)(cards.getLayout());
+	        cl.show(cards, "madlibsong");
+	        }
+	    });
+	startPage.add(comboBox);
+}
+/**
+* Create the button for choosing a random song
+*/
+private void makeRandomButton() {
+	JButton btnRandom = new JButton("Random");
+	btnRandom.setBounds(143, 215, 117, 29);
+	btnRandom.addActionListener(new ActionListener() {
+		public void actionPerformed(ActionEvent e) {
+		       chosenSong.randomize();
+		       chosenSong.initializeLib();	            
+		       game();
+	           CardLayout cl = (CardLayout)(cards.getLayout());
+	           cl.show(cards, "madlibsong");
+		 }
+	});
+	startPage.add(btnRandom);
+}
+/**
+* Create the madlib panel
+*/
+private void makeMadLibPage() {
+		JPanel panel2 = new JPanel();
+		scroller = new JScrollPane(panel2);
+		madlibPage = panel2;
+		frmSonglibs.setPreferredSize(new Dimension(450, 200));
+		frmSonglibs.getContentPane().add(scroller, "madlibsong");
+		panel2.setLayout(null);
+}
+/**
+* Create the finished song panel
+*/
+private void makeFinishedPage() {
+	    finishedPage = new JPanel();
+	    frmSonglibs.getContentPane().add(finishedPage, "result");
+	    finishedPage.setLayout(null);
+}
+
+}
---
+++ src/gameLogic.java
@@ -0,0 +1,190 @@
+
+import java.io.BufferedReader;
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.FileOutputStream;
+import java.io.FileReader;
+import java.io.IOException;
+import java.util.Enumeration;
+import java.util.HashMap;
+import java.util.Properties;
+import java.util.Random;
+import java.util.Scanner;
+
+public class gameLogic {
+	private BufferedReader reader;
+	private Properties songChoices;
+	private String origSong;
+	String songName;
+	String song;
+	HashMap<String, String> replaceWords;
+	
+	/**
+	 * Initialize values
+	 */
+	public gameLogic(){
+		songChoices = new Properties();
+		song ="";
+		load();
+	}
+	/**
+	 * Load properties file of song options
+	 */
+	public void load(){
+		try {
+			songChoices.load(new FileInputStream("songChoices.properties"));
+		} catch (IOException e) {
+			create();
+		}
+	}
+	/**
+	 * For testing purposes -- Hardcoded test songs
+	 */
+	public void createTest() {
+		try {
+			songChoices.setProperty("Mary Had A Little Lamb", "adlib1.txt");
+			songChoices.setProperty("Birthday Song", "birthdaysong.txt");
+			songChoices.setProperty("Call Me Maybe", "callmemaybe.txt");
+			songChoices.store(new FileOutputStream("songChoices.properties"), null);
+		} catch (IOException e){
+			System.err.print("Error");
+		}
+	}
+	/**
+	 * Create properties file if it doesn't exist
+	 */
+	public void create(){
+		String cd = System.getProperty("user.dir").toString();
+		cd+="/songs";
+		final File folder = new File(cd);
+		File[] songs = folder.listFiles();
+		for (File file : songs) {
+			if (file.getName().startsWith(".")){
+				continue;
+			}
+			songChoices.setProperty(file.getName().substring(0, file.getName().length() - 4), file.toString());
+		}
+	}
+	/**
+	 * For testing purposes -- 1. list the options from properties file
+	 * 2. Choose song option through command line
+	 */
+	public void listOptions(){
+		Enumeration<?> list = songChoices.propertyNames();
+		System.out.println("Type in the name of a SONGLIB song from the following:");
+		while (list.hasMoreElements()){
+			System.out.println(list.nextElement());
+		}
+		System.out.println();
+		Scanner in = new Scanner(System.in);
+		String choice = null;
+		while (choice == null) {
+			System.out.print("song: ");
+			choice = in.nextLine();
+			songName = choice;
+			choice = songChoices.getProperty(choice);
+			if (choice != null){
+				try {
+					reader = new BufferedReader(new FileReader(choice));
+				} catch (Exception e){
+				}
+			} else {
+				System.out.println("Incorrect SONGLIB name!");
+			}
+		}
+	}
+	/**
+	 * Return a string array of song options; first option left blank
+	 */
+	public String[] getOptions(){
+		Enumeration<?> list = songChoices.propertyNames();
+		String[] toRtn = new String[songChoices.size()+1];
+		int i = 1;
+		toRtn[0] = "";
+		while (list.hasMoreElements()){
+			toRtn[i] = list.nextElement().toString();
+			i++;
+		}
+		return toRtn;
+	}
+	/**
+	 * Select the song choice, and its text file from the property file
+	 */
+	public void choose(String choice){
+		songName = choice;
+		choice = songChoices.getProperty(choice);
+		try {
+			reader = new BufferedReader(new FileReader(choice));
+		} catch (Exception e){
+		}
+	}
+	/**
+	 * Read song file. Put words to be replaced in hashmap.
+	 */
+	public void initializeLib(){
+		try {
+			String line;
+			song="";
+			replaceWords = new HashMap<String, String>();
+			while (!(line = reader.readLine()).equals("***")){
+				int space = line.indexOf(" ");
+				replaceWords.put(line.substring(0, space), line.substring(space+1));
+			}
+			while ((line = reader.readLine()) != null){
+				song += line + "\n";
+			}
+			origSong = song;
+		} catch (Exception e){
+			
+		}
+	}
+	/**
+	 * Choose a random song
+	 */
+	public void randomize(){
+		Enumeration<?> list = songChoices.propertyNames();
+		Random rn = new Random();
+		int i = rn.nextInt(songChoices.size());
+		String choice="";
+		for (int z=0; z<=i; z++){
+			choice = list.nextElement().toString();
+		}
+		choose(choice);
+	}
+	/**
+	 * Reset the song to its original state
+	 */
+	public void reset(){
+		song = origSong;
+	}
+	/**
+	 * For testing purposes -- 1. Get user-entered words
+	 * 2. Place user-entered words into song
+	 */
+	public void getUserWords(){
+		System.out.println();
+		System.out.println("Enter a word for each type listed");
+		Scanner in = new Scanner(System.in);
+		String userWord;
+		for (String word : replaceWords.keySet()) {
+			System.out.print(replaceWords.get(word) + ": ");
+			userWord = in.nextLine();
+			song = song.replaceAll(word, userWord);
+		}
+		
+	}
+	/**
+	 * Replace words to be replaced with user-entered word
+	 */
+	public void replaceWord(String word, String userWord){
+		song = song.replaceAll(word, userWord);
+	}
+	/**
+	 * For testing purposes -- Print song with user substitutions
+	 */
+	public void printResult(){
+		System.out.println();
+		System.out.println(songName);
+		System.out.println(song);
+	}
+}
